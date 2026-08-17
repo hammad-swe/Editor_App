58602d494fdfd3deb79f6412b2fdacc0104dba0d
Hammad Ali <hammadali78056@gmail.com>
2026-08-17 12:53:19 +0500

    Feat: Add Splash & GetStarted screens, assets, .gitignore
    Introduce SplashViewController and GetstartedViewController with corresponding XIBs, add two image assets (shapes / shapes1), and update SceneDelegate to create the UIWindow, set a UINavigationController with SplashViewController as root, and make it key & visible. Also add a .gitignore with common Xcode, SwiftPM, CocoaPods, Carthage, and temporary file rules.

---
+++ Editor_App/Screens/GetStarted/GetstartedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GetstartedViewController.swift
+//  Editor_App
+//
+//  Created by Hammad Ali on 17/08/2026.
+//
+
+import UIKit
+
+class GetstartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+       
+    }
+
+
+}
